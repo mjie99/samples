24cf411cf09cd6bcaf7eb9cae415e978e9da51f2
mjie99 <mingjiekhaw@gmail.com>
2021-03-18 15:14:02 +0800

    first commit

---
+++ samples/src/demo/Location.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Location {
+	
+	private String location;
+	
+	public void setLocation(String location)
+	{
+		this.location=location;
+	}
+	
+
+	public String getLocation() {
+		return location;
+	} 
+	
+
+}
