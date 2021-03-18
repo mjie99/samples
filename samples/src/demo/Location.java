6a8d5a6df02513de5def4d2093422c57870fc6c0
mjie99 <mingjiekhaw@gmail.com>
2021-03-18 15:18:40 +0800

    third commit

--- samples/src/demo/Location.java
+++ samples/src/demo/Location.java
@@ -3,18 +3,7 @@ package demo;
 public class Location {
 	
 	private String location;
-	private String name;
 	
-	
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 
 	public void setLocation(String location)
